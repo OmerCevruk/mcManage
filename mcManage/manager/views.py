8d24324badaf6f0666a4f573053c1445159ee640
OmerCevruk <omer.cevrukk@gmail.com>
2024-04-14 17:54:22 +0300

    using screen for better management

--- mcManage/manager/views.py
+++ mcManage/manager/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout,login,authenticate
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 import subprocess
+import os
 
 class CustomLoginView(LoginView):
     redirect_authenticated_user = True
@@ -18,27 +19,41 @@ def logout_view(request):
 def main_page(request):
     return render(request, 'main_page.html')
 
+def server_status():
+    # Check if the server process is running
+    result = os.popen('screen -ls').read()
+    if '.minecraft'  in result:
+        print("Server is running.")
+        return True
+    else:
+        print("Server is not running.")
+        return False
+    #pid_check = os.system("pgrep -f 'minecraft_server.jar' >/dev/null")
+    #return pid_check == 0
+
+def server_command(command):
+    # Send a command to the server using screen
+    os.system(f"screen -S minecraft -p 0 -X stuff '{command}\r'")
+
 def start_minecraft_server(request):
-    # Command to start Minecraft server (adjust path and command as needed)
-    command = '/path/to/minecraft/server/start_script.sh'
-    
     try:
-        # Execute the command to start the Minecraft server
-        subprocess.Popen(command, shell=True)
-        # Optionally, you can redirect the user to another page after starting the server
-        return render(request, 'main_page.html', {'message': 'Minecraft server started successfully.'})
+        if not server_status():
+            os.chdir("/root/1.0.980TekxitPiServer/")
+            os.system('screen -dmS "minecraft" java -Xmx4G -Xms4G -jar forge-1.12.2-14.23.5.2860.jar nogui')
+            return render(request, 'main_page.html', {'message': 'Minecraft server started successfully.'})
+        else:
+            return render(request, 'main_page.html', {'message': 'server is already running .'})
     except Exception as e:
         # Handle any errors that occur during command execution
         return render(request, 'main_page.html', {'error': str(e)})
+
 def stop_minecraft_server(request):
-    # Command to stop Minecraft server (adjust path and command as needed)
-    command = '/path/to/minecraft/server/stop_script.sh'
-    
     try:
-        # Execute the command to stop the Minecraft server
-        subprocess.run(command, shell=True)
-        # Optionally, you can redirect the user to another page after stopping the server
-        return render(request, 'main_page.html', {'message': 'Minecraft server stopped successfully.'})
+        if server_status():
+            server_command('stop')
+            return render(request, 'main_page.html', {'message': 'Minecraft server stopped successfully.'})
+        else:
+            return render(request, 'main_page.html', {'message': 'server not running.'})
     except Exception as e:
         # Handle any errors that occur during command execution
         return render(request, 'main_page.html', {'error': str(e)})
